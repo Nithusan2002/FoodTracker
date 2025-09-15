c031f776bc1a97c6d5a14fbeabc02b47c4af07c1
Nithusan <nithusak@ifi.uio.no>
2025-09-15 20:44:54 +0200

    First commit - FoodTracker

---
+++ FoodTracker/FoodTracker/FoodTrackerApp.swift
@@ -0,0 +1,17 @@
+//
+//  FoodTrackerApp.swift
+//  FoodTracker
+//
+//  Created by Nithusan Krishnasamymudali on 15/09/2025.
+//
+
+import SwiftUI
+
+@main
+struct FoodTrackerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ FoodTracker/FoodTrackerTests/FoodTrackerTests.swift
@@ -0,0 +1,17 @@
+//
+//  FoodTrackerTests.swift
+//  FoodTrackerTests
+//
+//  Created by Nithusan Krishnasamymudali on 15/09/2025.
+//
+
+import Testing
+@testable import FoodTracker
+
+struct FoodTrackerTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
